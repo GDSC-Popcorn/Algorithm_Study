4ff524a1b7f47a6961b03abab7d91da2a98d730e
MinwooJe <gpatration77@gmail.com>
2025-06-03 19:55:38 +0900

    1149_RGB거리

--- MinwooJe/4주차_DP/1149_RGB거리.swift
+++ MinwooJe/4주차_DP/1149_RGB거리.swift
@@ -1,1 +1,19 @@
+let rowCount = Int(readLine()!)!
+var house = [[Int]]()
+var table = Array(repeating: [0, 0, 0], count: rowCount)
+
+for _ in 0..<rowCount {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    house.append(input)
+}
+
+table[0] = house[0]
+
+for i in 1..<rowCount {
+    table[i][0] = house[i][0] + min(table[i - 1][1], table[i - 1][2])
+    table[i][1] = house[i][1] + min(table[i - 1][0], table[i - 1][2])
+    table[i][2] = house[i][2] + min(table[i - 1][0], table[i - 1][1])
+}
+
+print(table[rowCount - 1].min()!)
 
