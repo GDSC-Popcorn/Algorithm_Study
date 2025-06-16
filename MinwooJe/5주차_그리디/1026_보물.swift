4d348b0209b095b2f4e2a0a30bfcf997ce42b310
MinwooJe <gpatration77@gmail.com>
2025-06-16 20:57:33 +0900

    1026_보물

---
+++ MinwooJe/5주차_그리디/1026_보물.swift
@@ -0,0 +1,18 @@
+// A: [Int], B: [Int] -> 길이 n
+// 인덱스끼리의 곱
+// S의 값을 가장 작게 A의 수 재배열. B는 그대로
+// 두 수의 곱은 두 수의 차가 작을 수록 작아짐.
+// Let a + b = 10 (일정)
+// put: a = 1, b = 9 -> a * b = 9
+// put: a = 2, b = 8 -> a * b = 16
+// put: a = 3, b = 7 -> a * b = 21
+// put: a = 4, b = 6 -> a * b = 24
+// put: a = 5, b = 5 -> a * b = 25
+
+// |A[i] - B[i]| 가 작도록 A 순서 바꾸면 됨.
+// greedy: B의 가장 큰 원소 * A의 가장 작은 원소
+
+let len = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map { Int($0)! }
+let seq = readLine()!.split(separator: " ").map { Int($0)! }
+print(zip(arr.sorted(), seq.sorted { $0 > $1 }).reduce(0) { $0 + $1.0 * $1.1 })
